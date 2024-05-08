ea5fa182ff5261340c6b6ab8ae986f573cd7b570
Joe Cin Ng <joe5763481@gmail.com>
2024-05-08 11:15:08 +1000

    pdf download

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,26 @@ $(document).ready(function() {
     $('select').formSelect();
     initializeModal('addModal', "Add Medication");
     initializeModal('editModal', "Edit Medication");
-
+    $('#downloadbtn').click(()=>{ 
+        downloadpdf();
+    })
 });
 
+function downloadpdf() {
+    var doc = new jsPDF();
+
+    // Add title
+    doc.setFontSize(16);
+    doc.text("Medication Records", 14, 15);
+    
+    // Add text
+    doc.setFontSize(12);
+    doc.text("Patient: John Doe", 14, 25); // Text position
+
+    doc.autoTable({
+        html: '#table',
+        startY: 35
+    })
+
+    doc.save('table.pdf')
+}
