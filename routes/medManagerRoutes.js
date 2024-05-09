01770f526a6d8dc2eea24d4ebb3882e49ccd54a1
Maxie Cletus <maxiecletus@gmail.com>
2024-05-09 11:52:47 +1000

    Added the navbar and routes

--- routes/medManagerRoutes.js
+++ routes/medManagerRoutes.js
@@ -3,8 +3,16 @@ var express = require("express");
 var router = express.Router();
 const path = require("path"); // Import the path module
 
-router.get('/index', function (req, res, html) {
+router.get('/', function (req, res, html) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+router.get('/page1', function(req, res, html){
     res.sendFile(path.join(__dirname, '../public/views/med_manager/index.html'));
 });
-
+router.get('/page2', function(req, res, html){
+    res.sendFile(path.join(__dirname, '../public/views/dashboard/index.html'));
+});
+router.get('/page3', function(req, res, html){
+    res.sendFile(path.join(__dirname, '../public/views/symptom_checker/index.html'));
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
+const medManagerRoutes = require('./routes/medManagerRoutes');
 
 // Middleware
 app.use(express.json());
@@ -9,14 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Route to render the index.ejs on the root URL
 app.get('/', (req, res) => {
-    res.render('index', { medications: [] }); // You need to adjust the data passed to the template as per your actual data handling
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // You need to adjust the data passed to the template as per your actual data handling
 });
-
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-const medManagerRoutes = require('./routes/medManagerRoutes');
 app.use('/medManager', medManagerRoutes);
 
 server.listen(3000, () => console.log('Server running on http://localhost:3000'));
