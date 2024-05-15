65a60036fd4e7feb136965056912ea75e66ebe32
Joe Cin Ng <joe5763481@gmail.com>
2024-05-15 13:07:38 +1000

    APIs for adding and retrieving medications
    - added functions in user.js model to add and retrieve data from the db collection
    
    - added functions in medController.js to add and retrieve data using user.js model
    
    - added api routes in medManagerRoutes.js
    
    - APIs have been tested successfully using Postman

--- routes/medManagerRoutes.js
+++ routes/medManagerRoutes.js
@@ -1,10 +1,15 @@
 
 var express = require("express");
 var router = express.Router();
+var medManagerController = require("../controllers/medManagerController");
 const path = require("path"); // Import the path module
 
 router.get('/index', function (req, res, html) {
     res.sendFile(path.join(__dirname, '../public/views/med_manager/index.html'));
 });
+
+router.get('/getMeds', medManagerController.getAllMedication);
+
+router.post('/addMed', medManagerController.addMedication);
 
 module.exports = router;
