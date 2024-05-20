a0d3635ab587bd63aefb8dd70adc667e169c5858
EagleMarcus <132487471+EagleMarcus@users.noreply.github.com>
2024-05-21 03:42:36 +1000

    Refactored code: removed comments and improved readability

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -14,24 +14,5 @@ router.get('/', function (req, res) {
     path.join(__dirname, '../public/views/landingpage/index - JoinUs.html')
   );
 });
-// router.get('/api/user', async function(req, res){
-//   let result = await controller.getAllCats(req,res);
-//   res.json({statusCode: 200, message: 'success', data: result});
-// });
 
 module.exports = router;
-
-
-/*
-var express = require('express');
-var router = express.Router();
-const path = require('path');
-
-router.get('/', function (req, res) {
-  res.sendFile(
-    path.join(__dirname, '../public/views/symptom_checker/index.html')
-  );
-});
-
-module.exports = router;
-*/
