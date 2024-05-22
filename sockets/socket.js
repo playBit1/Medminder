3c5367cf88824ab1c2d6925f7565c8a62d0f64a4
maxwellc21 <maxiecletus@gmail.com>
2024-05-22 10:25:29 +1000

    socket commited

---
+++ sockets/socket.js
@@ -0,0 +1,17 @@
+const socketIO = require('socket.io');
+
+const initializeSocket = (server) => {
+  const io = socketIO(server);
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  return io;
+};
+
+module.exports = initializeSocket;
