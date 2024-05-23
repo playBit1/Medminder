d5b91d992fe3d4db5982b9ff0eb196d63a281a5d
maxwellc21 <maxiecletus@gmail.com>
2024-05-24 03:50:10 +1000

    notofication endpoints created and socket initialisation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const cookieParser = require('cookie-parser');
 const { runDB } = require('./dbConnection');
+const socketNotifications = require('./sockets/notifications');
 
 // Middleware
 app.use(express.json());
@@ -23,6 +24,7 @@ const symptomCheckerRoutes = require('./routes/symptom_checker_routes');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const dashboardRoutes = require('./routes/dashboard_routes');
+const notificationsRouter = require('./routes/notifications');
 
 app.use('/medManager', medManagerRoutes);
 
@@ -33,6 +35,11 @@ app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
 app.use ('/dashboard', dashboardRoutes);
+
+app.use ('/notify', notificationsRouter);
+
+// Initialize socket.io
+socketNotifications(server);
 
 server.listen(3000, () => {
   runDB();
