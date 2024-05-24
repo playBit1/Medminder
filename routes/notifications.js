f4d8262c02479bcea814f28552e431a73bb4f169
maxwellc21 <maxiecletus@gmail.com>
2024-05-25 02:22:44 +1000

    profile route created

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
 const { getNotifications, updateNotificationStatus } = require('../controllers/notifications');
 const { authenticate } = require('../controllers/user_auth');
 
-// Serve the HTML file for notifications
-//router.get('/', authenticate, (req, res) => {
-  //  res.sendFile(path.join(__dirname, '../public/views/nav/notifytest.html'));
-//});
-//router.get('/', authenticate, getNotifications);
-
-// Separate endpoint for fetching notifications
 router.get('/', authenticate, getNotifications);
 
-router.post('/update', authenticate, updateNotificationStatus);
+router.post('/:action/:id', authenticate, async (req, res) => {
+  const { action, id } = req.params;
+  const status = action === 'taken' ? 'Taken' : 'Skipped';
+
+  try {
+    req.body.notificationId = id;
+    req.body.status = status;
+    await updateNotificationStatus(req, res);
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Failed to update notification status' });
+  }
+});
 
 module.exports = router;
