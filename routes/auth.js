c9e7af4420e0107dc78825bd040c8bd8a827c8c6
playBit1 <87233951+playBit1@users.noreply.github.com>
2024-05-26 19:26:03 +1000

    merge bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login } = require('../controllers/auth');
+const { register, login, logout } = require('../controllers/auth');
 
 const router = express.Router();
 
