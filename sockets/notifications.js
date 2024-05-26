da2c5c5e8495ee291faf516bdbe977daf6444f0c
maxwellc21 <maxiecletus@gmail.com>
2024-05-26 13:54:05 +1000

    Notification socket updated

--- sockets/notifications.js
+++ sockets/notifications.js
@@ -1,28 +1,50 @@
-const socketIo = require('socket.io');
 const User = require('../models/User');
 
-module.exports = (server) => {
-  const io = socketIo(server);
-
+module.exports = (io) => {
   io.on('connection', (socket) => {
-    const userId = socket.handshake.query.userId;
+    console.log('New client connected');
 
-    // Join a room specific to the user
-    socket.join(userId);
+    // A set to keep track of sent notifications
+    const sentNotifications = new Set();
 
-    // Function to send notifications to the user
-    const sendNotification = async () => {
-      const user = await User.findById(userId);
-      if (user) {
-        const notifications = Array.from(user.user_notifications.values());
-        socket.emit('notification', notifications);
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+
+    // Emit notifications due in the next 5 minutes
+    const checkNotifications = async () => {
+      try {
+        const users = await User.find({});
+        const now = new Date();
+
+        users.forEach(async (user) => {
+          const notifications = user.user_notifications;
+          if (notifications && notifications instanceof Map) {
+            const upcomingNotifications = Array.from(notifications.values()).filter(notification => {
+              const notificationTime = new Date(`${notification.date}T${notification.time}`);
+              const timeDiff = (notificationTime - now) / 1000 / 60; // difference in minutes
+              const notificationKey = `${user._id}-${notification.date}-${notification.time}`; // Unique key for each notification
+              return notification.status === 'Not taken' && timeDiff <= 2 && timeDiff >= 0 && !sentNotifications.has(notificationKey);
+            });
+
+            if (upcomingNotifications.length > 0) {
+              socket.emit('notifications', upcomingNotifications);
+              console.log('Notifications emitted:', upcomingNotifications); // Log emitted notifications
+
+              // Add the notifications to the set of sent notifications
+              upcomingNotifications.forEach(notification => {
+                const notificationKey = `${user._id}-${notification.date}-${notification.time}`;
+                sentNotifications.add(notificationKey);
+              });
+            }
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching notifications:', error);
       }
     };
 
-    sendNotification();
-
-    socket.on('disconnect', () => {
-      socket.leave(userId);
-    });
+    // Check for notifications every minute
+    setInterval(checkNotifications, 60000);
   });
 };
